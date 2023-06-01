a1a610f72b5238c2e97220828fd7ab70530ab13e
Phil-Mart <cooldrumsmusic@gmail.com>
2023-05-31 18:28:28 -0700

    initial commit

---
+++ healthcare.sql
@@ -0,0 +1,13 @@
+Create Table "Independent_Medical_Review_IMR_Determinations_Trend" (
+	"Reference ID" Varchar,
+	"Report Year" INT,
+	"Diagnosis Category" Varchar,
+	"Diagnosis Sub Category" Varchar,
+	"Treatment Category" Varchar,
+	"Treatment Sub Category" Varchar,
+	"Determination" Varchar,
+	"Type" Varchar,
+	"Age Range" Varchar,
+	"Patient Gender" Varchar,
+	"Findings" Varchar
+);
---
+++ healthcare_query.sql
@@ -0,0 +1,15 @@
+Select 
+	diagnosis_category,
+	diagnosis_sub_category,
+	treatment_category,
+	treatment_sub_category,
+	determination,
+	type,
+	age_range,
+	patient_gender
+FROM urgentcare
+Where report_year = 2016;
+
+export to csv ("")
+
+select * from urgentcare; 
